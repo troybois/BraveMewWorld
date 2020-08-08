d3a805db7fc1778680e7738ac5844a6d3f6511ce
Jared Flores <jared_flores@cs.ucsb.edu>
2020-08-07 21:05:28 -0700

    Adding Basic Networking

--- docs/js/conn.js
+++ docs/js/conn.js
@@ -1,1 +1,36 @@
 // conn.js
+function init_conn( isDM, msg_handler ) {
+	var URL = "ws://34.209.226.143:25565";
+	
+	var player = !isDM,
+		ws = new WebSocket( URL );;
+
+	function on_reopen() {
+
+	}
+
+	function on_open() {
+		if( !player ) {
+			ws.send( "host" );
+		}
+	}
+
+	function on_msg( evt ) {
+		msg_handler( evt );
+	}
+
+	function on_close( evt ) {
+
+	}
+
+	function send( msg ) {
+		ws.send( msg );
+	}
+
+	ws.addEventListener( "open", on_open );
+	//ws.addEventListener( "open", ws_reopen );
+	ws.addEventListener( "message", on_msg );
+	ws.addEventListener( "close", on_close );
+
+	window.send = send;
+}
--- docs/js/player.js
+++ docs/js/player.js
@@ -1,1 +1,6 @@
 // player.js
+function handle_msg( evt ) {
+	console.log( evt.data );
+}
+
+window.init_conn( false, handle_msg );
