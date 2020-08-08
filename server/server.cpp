475c6fa3880a1fa07697d1008116d6467a582471
Debian <admin@ip-172-26-2-89.us-west-2.compute.internal>
2020-08-08 01:08:44 +0000

    Initial Server

--- server/server.cpp
+++ server/server.cpp
@@ -1,4 +1,34 @@
-// server.cpp
+#include "server.h"
+
+using namespace std;
+
+us_listen_socket_t* listener;
+
 int main() {
+	uWS::App().ws<BMWClient>( "/*", {
+		.compression = uWS::SHARED_COMPRESSOR,
+		.maxPayloadLength = 16 * 1024 * 1024,
+		.idleTimeout = 10,
+		.maxBackpressure = 1 * 1024 * 1024,
+		.open = []( auto* ws ) {
+		
+		},
+		.message = []( auto* ws, string_view msg, uWS::OpCode oc ) {
+		
+		},
+		.drain = []( auto* ws ) {
+		},
+		.ping = []( auto* ws ) {
+		},
+		.pong = []( auto *ws ) {
+		},
+		.close = []( auto* ws, int code, string_view message ) {
+		}
+	} ).listen( 25565, []( auto* token ) {
+		listener = token;
+		if( token ) {
+			cout << "Listening on port " << 25565 << endl;
+		}
+	} ).run();
 	return 0;
 }
--- server/server.h
+++ server/server.h
@@ -1,1 +1,13 @@
-//server.h
+#ifndef BMW_H
+#define BMW_H
+
+#include <iostream>
+#include <string>
+
+#include "App.h"
+
+struct BMWClient {
+
+};
+
+#endif
