8a9d3a12e9d05bf41a9232154f9dc47227098ece
Andrew Setiadi <asetiadi@ucsd.edu>
2020-08-08 02:00:08 +0000

    fffjf

---
+++ experiments/Javascript/simp.js
@@ -0,0 +1,17 @@
+function demo() {
+const sampler = new Tone.Sampler({
+	urls: {
+		"C4": "C4.mp3",
+		"D#4": "Ds4.mp3",
+		"F#4": "Fs4.mp3",
+		"A4": "A4.mp3",
+	},
+	release: 1,
+	baseUrl: "https://tonejs.github.io/audio/salamander/",
+}).toDestination();
+
+Tone.loaded().then(() => {
+	sampler.triggerAttackRelease(["Eb4", "G4", "Bb4"], 4);
+})
+
+document.getElementById( "start" ).addEventListener( "click", demo );
