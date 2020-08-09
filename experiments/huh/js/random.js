ce061636442f5c9220e33f424b98a71fe9cb0c5c
Andrew Setiadi <asetiadi@ucsd.edu>
2020-08-08 17:29:43 -0700

    mewsic

--- experiments/huh/js/random.js
+++ experiments/huh/js/random.js
@@ -1,11 +1,14 @@
 var PRNG_VARS = {};
 PRNG_VARS["x"] = 2348057239;
 
-function prng(rngType, range, params) {
+function prng(rngType, range, seed) {
 	PRNG_VARS["a"] = range;
+	if(seed != undefined && seed != null) {
+		PRNG_VARS["x"] = seed;
+	}
 	switch(rngType) {
 		case "modular":
-			PRNG_VARS["x"] = (1101 * PRNG_VARS.x + 1102)%(1100);
+			PRNG_VARS["x"] = (7901 * PRNG_VARS.x + 9467)%(177013);
 			return PRNG_VARS.x % PRNG_VARS.a;
 	}
 }
